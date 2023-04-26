a2c94cd1b803413dd2f38218e94b1f61d893eed5
Haseeb-creator <mdhaseeb769@gmail.com>
2023-04-26 16:19:34 +0530

    updating footer image location

--- portfolio/frontend/src/PortfolioContianer/Home/Footer/Footer.js
+++ portfolio/frontend/src/PortfolioContianer/Home/Footer/Footer.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import './Footer.css'
 
-
-
 const Footer = () => {
 	return (
 		<div className="footer-container">
 			<div className="footer-parent">
-				<img src={require('../../../assets/Home/shape-bg.png')} alt="no internet connection 	" />
+				<img src={require('../../../img/Testimonial/shape-bg.png')} alt="no internet connection 	" />
 			</div>
 		</div>
 	)
