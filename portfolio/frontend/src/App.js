ae27520c321d19e3de2cdcbec54096272d134ba1
Haseeb-creator <mdhaseeb769@gmail.com>
2023-04-29 16:47:28 +0530

    server is created

--- portfolio/frontend/src/App.js
+++ portfolio/frontend/src/App.js
@@ -1,8 +1,12 @@
 import PortfolioContainer from "./PortfolioContianer/PortfolioContainer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
       <PortfolioContainer />
     </div>
   );
